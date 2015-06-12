1b04c70255d9004267d642eeb1b121295e692319
zhuhk <zhuhongk@gmail.com>
2015-06-12 18:59:43 +0800

    mod Makefile

--- cpp/t3.cpp
+++ cpp/t3.cpp
@@ -1,7 +1,9 @@
 #include <iostream>       // std::cout
 #include <thread>         // std::thread
 #include <future>         // std::promise, std::future
+#include <unistd.h>
 
+using namespace std;
 std::promise<int> prom;
 
 void print_global_promise () {
@@ -12,12 +14,15 @@ void print_global_promise () {
 
 int main () {
   std::thread th1(print_global_promise);
+  std::cout << "sleep 3s" <<endl;
+  sleep(1);
   prom.set_value(10);
   th1.join();
 
   prom = std::promise<int>();    // prom 被move赋值为一个新的 promise 对象.
 
   std::thread th2 (print_global_promise);
+  sleep(1);
   prom.set_value (20);
   th2.join();
 
