001df6018b184493ce202fc0c666fd42a7847416
Administrator <yuenyun@126.com>
2015-06-23 15:30:57 +0800

    use pyplot to gen figures

--- py/scikit_lr.py
+++ py/scikit_lr.py
@@ -1,6 +1,13 @@
 import sklearn.datasets
 import sklearn.linear_model
 import numpy.random
+import numpy as np
+from sklearn.preprocessing import label_binarize
+from sklearn.metrics import precision_recall_curve
+
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
 
 iris = sklearn.datasets.load_iris()
 
@@ -21,12 +28,32 @@ train_targets = iris.target[shuffleIdx [:sampleBoundary]]
 # Make the testing data
 test_features = iris.data[shuffleIdx[sampleBoundary:]]
 test_targets = iris.target[shuffleIdx[sampleBoundary:]]
+test_btargets = label_binarize(test_targets, classes=[0,1,2])
+n_classes = test_btargets.shape[1]
 
 # Train
 logisticRegression = sklearn.linear_model.LogisticRegression()
 logisticRegression.fit(train_features, train_targets)
 # Predict
-predict_targets = logisticRegression.predict(test_features) 
-predict_probs = logisticRegression.predict_proba(test_features) 
+test_predict_targets = logisticRegression.predict(test_features) 
+test_probs = logisticRegression.predict_proba(test_features) 
 print logisticRegression.score(test_features,test_targets)
+
+precision = dict()
+recall = dict()
+for i in range(n_classes):
+  precision[i], recall[i], _ = precision_recall_curve(test_btargets[:, i], test_probs[:, i])
+  #print precision[i]
+  #print recall[i]
+  #print  ""
+  plt.clf()
+  plt.plot(recall[i], precision[i], label='Precision-Recall curve')
+  plt.xlabel('Recall')
+  plt.ylabel('Precision')
+  plt.ylim([0.0, 1.05])
+  plt.xlim([0.0, 1.0])
+  plt.title('Precision-Recall example')
+  plt.legend(loc="lower left")
+#plt.show()
+  plt.savefig('myfig' + str(i))
 
