0705c93c64aa40b1dc4b05409df7b1659523ea76
hongkai1 <hongkai1@staff.sina.com.cn>
2017-05-24 14:50:04 +0800

    update

--- cpp/t_c11.cpp
+++ cpp/t_c11.cpp
@@ -765,9 +765,9 @@ void test_refstr(){
 int NumberOfSetBits(int i){
        // Java: use >>> instead of >>
        // C or C++: use uint32_t
-       i = i - ((i >> 1) & 0x55555555);
-            i = (i & 0x33333333) + ((i >> 2) & 0x33333333);
-                 return (((i + (i >> 4)) & 0x0F0F0F0F) * 0x01010101) >> 24;
+  i = i - ((i >> 1) & 0x55555555);
+  i = (i & 0x33333333) + ((i >> 2) & 0x33333333);
+  return (((i + (i >> 4)) & 0x0F0F0F0F) * 0x01010101) >> 24;
 }
 
 void test_bitcount(){
